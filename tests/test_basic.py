72594a4500d8240a5eb0064f38af8158a64e0148
Don Neill <neilljd@gmail.com>
2025-07-31 10:58:12 -0700

    Add pyproject.toml and basic tests

---
+++ tests/test_basic.py
@@ -0,0 +1,15 @@
+"""Perform basic tests."""
+import pytest
+from SPCe import SpceController
+
+def test_initialization():
+    """Test initialization."""
+    controller = SpceController()
+    assert not controller.connected
+
+def test_connection_fail():
+    """Test connection failure."""
+    with pytest.raises(Exception):
+        controller = SpceController()
+        controller.connect(host="127.0.0.1", port=50000)
+        assert not controller.connected
